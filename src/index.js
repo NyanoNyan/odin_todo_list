4ec3221c4119efcf3d59a7f89b6d8c8d39b72341
Nyano <nayan100@hotmail.co.uk>
2020-12-24 20:33:03 +0000

    Just added tasks which I need to do

--- src/index.js
+++ src/index.js
@@ -2,15 +2,23 @@ console.log('hello')
 
 import { changeProject } from './modules/view';
 import { getTask } from './modules/getData'
-import { storeData } from './modules/addData'
+import { storeData,addDOM } from './modules/addData'
 
 let dataStore ;
 let notesStorage = [];
-
+let test = addDOM();
 // Goes to different projects section
 changeProject()
 
+test.getData()
+
 getTask()
+
+// !!! Need to initialize and put the data which are stored in the local storage when
+// the page loads.
+
+// !!! Need to make a factory function that triggers of button deletion when checkbox
+// is ticked
 
 // dataStore = storeData()
 // dataStore.localStorageVal()
