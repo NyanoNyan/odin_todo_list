f5583877b1b336450211146ad324fbc629f8801c
nyano <nayan100@hotmail.co.uk>
2020-12-31 11:10:42 +0000

    Added getProjects and projectMaker modules. Set up localstorage for project names at the beginning with the check if there is no storage for it or if there is no key associated with it. Loaded in the repopulation of stored projects.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,40 @@
 import { viewChanger } from './modules/view';
-import { getTask } from './modules/getData';
-import { storeData,addDOM } from './modules/addData';
+import { getTask, getProjects } from './modules/getData';
+import { storeData,addDOM, projectMaker } from './modules/addData';
 import {deleteItems, completedTasks} from './modules/complete_and_delete';
 
 let dataStore ;
 let notesStorage = [];
 let mainView = viewChanger();
+
+let loadDefaultProjects = storeData();
 let repopulateData = addDOM();
+let repopulateProjects = projectMaker();
+
 let deleteAction = deleteItems();
 let checkboxComplete = completedTasks();
 // Goes to different projects section
 mainView.changeProject()
 
+
+
+
 // Repopulates the data from local storage when user refreshes.
+// loadDefaultProjects.storeDefaultProject()
+
+// If there is no key for storing project name and if nothing is inside local storage
+// initialize localstorage for project names.
+if (localStorage.length == 0 && localStorage.key(0) != '!projectNames!') {
+    localStorage['!projectNames!'] = JSON.stringify([]);
+}
+
+repopulateProjects.loadAllProjects();
 repopulateData.getData();
+
 
 // Gets the data from form submition
 getTask();
+getProjects();
 
 // markout the completed tasks
 checkboxComplete.markDone();
@@ -43,3 +61,10 @@ deleteAction.delTasks();
 
 
 // !!! Need to make this work for the second proejct section
+
+// !!! Add button to add more projects. Would need to make a new html with all of the things inside as
+// previous task-content id values.
+
+// !!! Also need to add date and time on when it's due and priority 
+
+// !!! How to make two divs not touch
