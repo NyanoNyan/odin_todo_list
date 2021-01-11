1c0b9c3ddaf1e84c073470b3a72ae404c04689b2
nyano <nayan100@hotmail.co.uk>
2021-01-11 15:22:34 +0000

    Changed format of the display. But mostly added a check if users enter a blank it won't create a project. Also asdded a custom message if it's left blank.

--- src/modules/getData.js
+++ src/modules/getData.js
@@ -1,175 +1,156 @@
-import Note from './notes'
-import { storeData, addDOM, projectMaker, checkerDomElement, addDate } from './addData'
-import {compareAsc, format} from 'date-fns'
-
+import Note from "./notes";
+import {
+  storeData,
+  addDOM,
+  projectMaker,
+  checkerDomElement,
+  addDate,
+} from "./addData";
+import { compareAsc, format } from "date-fns";
 
 function getTask() {
-    
-    let textText = document.getElementById('form-input');
-    
+  let textText = document.getElementById("form-input");
 
-    let storeDataTest = storeData();
-    let test = addDOM();
+  let storeDataTest = storeData();
+  let test = addDOM();
 
-    textText.addEventListener('keypress', gatherData);
-    
+  textText.addEventListener("keypress", gatherData);
 
-    function gatherData(e) {
+  function gatherData(e) {
+    // This is the project name variable
+    let currentProj = document.querySelector(".unique-current-project");
+    let parentElement = currentProj.id;
+    // old code
+    // let parentElement = textText.parentElement.parentElement.id;
+    let storeObj = { taskValue: "", projectName: parentElement };
 
-        // This is the project name variable
-        let currentProj = document.querySelector('.unique-current-project');
-        let parentElement = currentProj.id;
-        // old code
-        // let parentElement = textText.parentElement.parentElement.id;
-        let storeObj = {taskValue : '', projectName: parentElement}
+    if (e.code === "Enter") {
+      e.preventDefault();
 
-        if (e.code === 'Enter') {
-            e.preventDefault();
-            
-            storeObj.taskValue = textText.value;
-            clearText('form-input');
-            
-            
-            if (storeObj.taskValue != "") {
-                // console.log(storeObj.projectName)
+      storeObj.taskValue = textText.value;
+      clearText("form-input");
 
-                let note = Note(storeObj.projectName, storeObj.taskValue);
-                // console.log(`The note obj is ${note.noteDetails()}`)
-                storeDataTest.StorageVal(storeObj.taskValue, note.setupData());
+      if (storeObj.taskValue != "") {
+        // console.log(storeObj.projectName)
 
-                
-                test.getData()
-    
-            }
-           }
-        // Testing
-        // console.log(storeObj.taskValue!="");
-        // console.log(storeObj);
-        
+        let note = Note(storeObj.projectName, storeObj.taskValue);
+        // console.log(`The note obj is ${note.noteDetails()}`)
+        storeDataTest.StorageVal(storeObj.taskValue, note.setupData());
 
-        // !!! Feels like this will the main page, importing notes, making notes and 
-        // putting them in the local storage. !!!
-
-        // checking if the input is not blank, if it's not blank then send the data.
-
+        test.getData();
+      }
     }
+    // Testing
+    // console.log(storeObj.taskValue!="");
+    // console.log(storeObj);
 
+    // !!! Feels like this will the main page, importing notes, making notes and
+    // putting them in the local storage. !!!
 
-
+    // checking if the input is not blank, if it's not blank then send the data.
+  }
 }
-
 
 function getProjects() {
+  const test = document.getElementById("form-input-project");
 
-    let projectCreate = projectMaker();
-    let projectLocalStorage = storeData();
+  let projectCreate = projectMaker();
+  let projectLocalStorage = storeData();
 
-    let formProject = document.getElementById('project-form');
-    formProject.addEventListener('keypress', passData);
+  let formProject = document.getElementById("project-form");
+  formProject.addEventListener("keypress", passData);
 
+  function passData(e) {
+    if (e.code === "Enter") {
+      e.preventDefault();
 
-    function passData(e) {
+      if (e.target.value !== "") {
+        // listOfProjects.push(e.target.value);
+        // Stores project in local storage has name !projectNames!
+        projectLocalStorage.storeProjects(e.target.value);
 
-        if (e.code === 'Enter') {
-            e.preventDefault();
+        // Creates project list and everything in it such as tasks and layout of all the html
+        projectCreate.addProject(e.target.value);
 
-            // listOfProjects.push(e.target.value);
-            // Stores project in local storage has name !projectNames!
-            projectLocalStorage.storeProjects(e.target.value)
-            
-            // Creates project list and everything in it such as tasks and layout of all the html
-            projectCreate.addProject(e.target.value);
-
-            clearText('form-input-project');
-            
-
-        }
-
-
+        clearText("form-input-project");
+      }
+      else {
+        test.setCustomValidity("Please enter a Project name.");
+      }
     }
-
+  }
 }
 
-
-
-// Delete input from using DOM, aka used for 
+// Delete input from using DOM, aka used for
 // deleting a task when the enter has been pressed
-function clearText (id) {
-    document.getElementById(id).value = "";
+function clearText(id) {
+  document.getElementById(id).value = "";
 }
 
-function getDate (modalContent) {
+function getDate(modalContent) {
+  let storeData1 = storeData();
+  // Test with diff dates
+  let addDate1 = addDate();
 
-    let storeData1 = storeData();
-    // Test with diff dates
-    let addDate1 = addDate();
+  // let dateForm = document.getElementsByClassName('date-form');
+  // let modal = document.getElementById("myModal");
 
-    // let dateForm = document.getElementsByClassName('date-form');
-    // let modal = document.getElementById("myModal");
+  // console.log(modalContent.childNodes[1])
 
-    // console.log(modalContent.childNodes[1])
+  // Get the specific form that is inside the task element
+  let test_capture = modalContent.childNodes[1];
+  // console.log(test_capture)
 
-    // Get the specific form that is inside the task element
-    let test_capture = modalContent.childNodes[1];
-    // console.log(test_capture)
+  test_capture.addEventListener("submit", collectDate);
 
-    test_capture.addEventListener('submit', collectDate);
+  // Array.from(dateForm).forEach(function (dateForm1) {
+  //     dateForm1.addEventListener('submit', collectDate);
 
-    // Array.from(dateForm).forEach(function (dateForm1) {
-    //     dateForm1.addEventListener('submit', collectDate);
+  // });
 
-    // });
+  // Need to find out if the submit is the correct submit since it's looping through the whole array.
+  // if not need to get that specific clicked dom node form.
 
-    // Need to find out if the submit is the correct submit since it's looping through the whole array.
-    // if not need to get that specific clicked dom node form.
+  // Need the node.
 
-    // Need the node. 
+  // Should go, click task, open span, get the data fields of span which has submit, put event listener on that
 
-    // Should go, click task, open span, get the data fields of span which has submit, put event listener on that
-    
+  function collectDate(e) {
+    // alert('hello')
+    e.preventDefault();
 
-    function collectDate(e) {
-        // alert('hello')
-        e.preventDefault();
+    // Get the specific clicked butons date-date value
+    // console.log(e.target.childNodes)
+    let date_date = checkerDomElement("date-date", e.target.childNodes, "id")
+      .value;
 
-        // Get the specific clicked butons date-date value
-        // console.log(e.target.childNodes)
-        let date_date = checkerDomElement('date-date', e.target.childNodes, 'id').value;
+    // Need to get the specific date-date value
 
-        // Need to get the specific date-date value
+    // let date_date = document.getElementById('date-date').value;
+    let form_date = new Date(date_date);
+    let currentDate = new Date();
 
-        // let date_date = document.getElementById('date-date').value;
-        let form_date = new Date(date_date);
-        let currentDate = new Date();
-        
-        // console.log(date_date)
+    // console.log(date_date)
 
-        // Get the task name from the form, using parent nodes and child nodes, lol
-        let domListTasks = e.target.parentNode.parentNode.parentNode.childNodes;
-        let taskName = checkerDomElement('task-name', domListTasks, 'class').textContent;
-        let taskNameNode = checkerDomElement('task-name', domListTasks, 'class');
+    // Get the task name from the form, using parent nodes and child nodes, lol
+    let domListTasks = e.target.parentNode.parentNode.parentNode.childNodes;
+    let taskName = checkerDomElement("task-name", domListTasks, "class")
+      .textContent;
+    let taskNameNode = checkerDomElement("task-name", domListTasks, "class");
 
-        // console.log(taskNameNode);
-        storeData1.storeDateInfo(form_date, taskName);
+    // console.log(taskNameNode);
+    storeData1.storeDateInfo(form_date, taskName);
 
-        // Testing Get difference in dates
-        addDate1.domDiffDate(form_date, currentDate, taskNameNode);
+    // Testing Get difference in dates
+    addDate1.domDiffDate(form_date, currentDate, taskNameNode);
 
-        // Need to feed this data through addData to add the data in localStorage
-        // Then need to add this data in index.html using DOM
-        // Can also do days remaining.
-    }
-
+    // Need to feed this data through addData to add the data in localStorage
+    // Then need to add this data in index.html using DOM
+    // Can also do days remaining.
+  }
 }
 
-
-
-export {
-    getTask,
-    getProjects,
-    getDate
-} 
-
+export { getTask, getProjects, getDate };
 
 // Old code using less code.
 // addForm.addEventListener("keypress", function(event) {
@@ -179,7 +160,6 @@ export {
 //     }
 //   });
 
-
 // // Gather tasks from object and put them in list to prepare it for the local storage
 // function gatherTasks(mainStore, storeObj) {
 
@@ -187,12 +167,11 @@ export {
 //     for (const key in mainStore) {
 //         if (storeObj.projectName == key) {
 //             mainStore[key].push(storeObj.taskValue);
-            
+
 //         }
 //     }
 
 // }
-
 
 // let mainStore = {'Today': [],
 //                 'Project One': []}
